d88eb99b77870dc05cbb5a023ffeeb34081db4b9
sanket2701 <sanket.bafna@somaiya.edu>
2020-12-18 23:58:45 +0530

    Sidebar fixed

--- src/components/Sidebar/SidebarComponent.js
+++ src/components/Sidebar/SidebarComponent.js
@@ -1,69 +1,96 @@
-import React, { Component } from 'react';
-import { Nav, NavItem, NavbarBrand, Button } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import './SidebarComponent.css';
-import history from '../../redux/history';
+import React, { Component } from "react";
+import { Nav, NavItem, NavbarBrand, Button } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import "./SidebarComponent.css";
+import history from "../../redux/history";
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.handleLogout = this.handleLogout.bind(this);
-    }
+  handleLogout() {
+    localStorage.removeItem("username");
+    // localStorage.removeItem('token');
+    history.push("/login");
+    window.location.reload(true);
+  }
 
-    handleLogout() {
-        localStorage.removeItem('username');
-        // localStorage.removeItem('token');
-        history.push('/login');
-        window.location.reload(true);
-    }
-
-    render() {
-        var username = localStorage.getItem('username');
-        return (
-            <div>
-                <Nav className="sidebar">
-                    <NavbarBrand className="sidebar-title">
-                        <h1>Welcome,</h1>
-                        <h3>{username}</h3>
-                    </NavbarBrand>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/home">Dashboard</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/personaldetails">Personal Details</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/academics">Academics</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/internships">Internships</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/projectdetails">Project Details</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/onlinecertifications">Online Certifications</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/co-curriculars">Co-Curriculars</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/extra-curriculars">Extra-Curriculars</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/attendance">Attendance</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <NavLink className="nav-link sidebar-text" to="/settings">Settings</NavLink>
-                    </NavItem>
-                    <NavItem className="sidebar-item">
-                        <Button className="nav-link sidebar-text" onClick={this.handleLogout}>Logout</Button>
-                    </NavItem>
-                </Nav>
-            </div>
-        );
-    }
+  render() {
+    var username = localStorage.getItem("username");
+    return (
+      <div>
+        <Nav className="sidebar">
+          <NavbarBrand className="sidebar-title">
+            <h1>Welcome,</h1>
+          </NavbarBrand>
+          <h3 className="sidebar-title">{username}</h3>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/home">
+              Dashboard
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/personaldetails">
+              Personal Details
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/academics">
+              Academics
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/internships">
+              Internships
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/projectdetails">
+              Project Details
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink
+              className="nav-link sidebar-text"
+              to="/onlinecertifications"
+            >
+              Online Certifications
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/co-curriculars">
+              Co-Curriculars
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/extra-curriculars">
+              Extra-Curriculars
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/attendance">
+              Attendance
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <NavLink className="nav-link sidebar-text" to="/settings">
+              Settings
+            </NavLink>
+          </NavItem>
+          <NavItem className="sidebar-item">
+            <Button
+              className="nav-link sidebar-text"
+              onClick={this.handleLogout}
+            >
+              Logout
+            </Button>
+          </NavItem>
+        </Nav>
+      </div>
+    );
+  }
 }
 
-export default Sidebar
+export default Sidebar;
